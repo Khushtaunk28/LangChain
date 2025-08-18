0a5671e22e2963c1305524519c10fba8eebec1a9
Khushtaunk28 <Khush.taunk@gmail.com>
2025-08-18 23:05:05 +0530

    learnig chains basics

---
+++ chains/chains_basics.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+
+load_dotenv()
+
+model=GoogleGenerativeAI(model="gemini-2.5-flash")
+# Define prompt templates (no need for separate Runnable chains)
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a facts expert who knows facts about {animal}."),
+        ("human", "Tell me {fact_count} facts."),
+    ]
+)
+
+#create a chain
+
+chain=prompt_template | model | StrOutputParser()
+# Run the chain
+result = chain.invoke({"animal": "elephant", "fact_count": 1})
+
+# Output
+print(result)
